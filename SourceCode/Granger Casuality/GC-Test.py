2fab445935d6b81ce7cf00b65e1cb746a682145a
iqrazaf <105370998+iqrazaf@users.noreply.github.com>
2022-05-26 22:11:24 +0200

    Update GC-Test.py

--- SourceCode/Granger Casuality/GC-Test.py
+++ SourceCode/Granger Casuality/GC-Test.py
@@ -1,7 +1,18 @@
-#Example
+#Test Example 01
 import pandas as pd
 from statsmodels.tsa.stattools import grangercausalitytests
 f = './loadtest.csv'
 P = pd.read_csv(f)
 #perform Granger-Causality test
 grangercausalitytests(P[['carts', 'front-end']], 4) # log value=1-4 
+
+
+---------------------------------------------------------------------
+
+#Test Example 02 : For testing Reverse Correlation 
+import pandas as pd
+from statsmodels.tsa.stattools import grangercausalitytests
+f = './loadtest.csv'
+P = pd.read_csv(f)
+#perform Granger-Causality test
+grangercausalitytests(P[['front-end', 'carts']], 4) # log value=1-4 
