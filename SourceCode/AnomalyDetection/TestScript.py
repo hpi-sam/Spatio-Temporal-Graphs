e0d68843b16806b4636956c860b82f2b4500ce91
iqrazaf <105370998+iqrazaf@users.noreply.github.com>
2022-05-26 22:14:15 +0200

    Update TestScript.py

--- SourceCode/AnomalyDetection/TestScript.py
+++ SourceCode/AnomalyDetection/TestScript.py
@@ -1,4 +1,4 @@
-#Example Code: 
+#Example Code for SPOT: 
 
 import numpy as np
 import matplotlib.pyplot as plt
@@ -15,6 +15,29 @@ u_init_data = (P['Time'] == '10/14/2021') | (P['Time'] == '10/20/2021')
 init_data = P['orders'][u_init_data].values
 q = 1e-5  			# risk parameter
 s = SPOT(q)  		# SPOT object
+s.fit(init_data,data) 	# data import
+s.initialize() 		# initialization step
+results = s.run() 	# run
+s.plot(results) 	# plot
+
+
+-----------------------------------------------------
+#Example Code for bidSPOT: 
+import numpy as np
+import matplotlib.pyplot as plt
+from spot import bidSPOT, dSPOT,  SPOT
+
+import pandas as pd
+f = './loadtest.csv'
+P = pd.read_csv(f)
+# stream
+u_data = (P['Time'] == '2/17/2022')| (P['Time'] == '3/2/2022')
+data = P['orders'][u_data].values
+# initial batch
+u_init_data = (P['Time'] == '10/14/2021') | (P['Time'] == '10/20/2021')
+init_data = P['orders'][u_init_data].values
+q = 1e-5  			# risk parameter
+s = bidSPOT(q)  		# bidSPOT object
 s.fit(init_data,data) 	# data import
 s.initialize() 		# initialization step
 results = s.run() 	# run
