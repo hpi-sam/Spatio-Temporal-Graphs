15adcee9ab92cac401e4ad417a5dcd62401dc381
iqrazaf <105370998+iqrazaf@users.noreply.github.com>
2022-06-07 22:33:42 +0200

    Update TestScript.py

--- SourceCode/AnomalyDetection/TestScript.py
+++ SourceCode/AnomalyDetection/TestScript.py
@@ -9,10 +9,10 @@ f = './loadtest.csv'
 P = pd.read_csv(f)
 # stream
 u_data = (P['Time'] == '2/17/2022')| (P['Time'] == '3/2/2022')
-data = P['orders'][u_data].values
+data = P['orders-db'][u_data].values
 # initial batch
-u_init_data = (P['Time'] == '10/14/2021') | (P['Time'] == '10/20/2021')
-init_data = P['orders'][u_init_data].values
+u_init_data = (P['Time'] == '10/14/2021') | (P['Time'] == '10/20/2021')| (P['Time'] == '10/19/2021')
+init_data = P['orders-db'][u_init_data].values
 q = 1e-5  			# risk parameter
 s = SPOT(q)  		# SPOT object
 s.fit(init_data,data) 	# data import
@@ -26,16 +26,16 @@ s.plot(results) 	# plot
 import numpy as np
 import matplotlib.pyplot as plt
 from spot import bidSPOT, dSPOT,  SPOT
-
 import pandas as pd
 f = './loadtest.csv'
 P = pd.read_csv(f)
 # stream
 u_data = (P['Time'] == '2/17/2022')| (P['Time'] == '3/2/2022')
-data = P['orders'][u_data].values
+data = P['orders-db'][u_data].values
 # initial batch
-u_init_data = (P['Time'] == '10/14/2021') | (P['Time'] == '10/20/2021')
-init_data = P['orders'][u_init_data].values
+u_init_data = (P['Time'] == '10/14/2021') | (P['Time'] == '10/20/2021')| (P['Time'] == '10/19/2021')
+init_data = P['orders-db'][u_init_data].values
+
 q = 1e-5 # risk parameter
 d = 10	# depth
 s = bidSPOT(q,d)  		# bidSPOT object
